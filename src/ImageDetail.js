725f30eb683944dd146ce85f9bdf119571183313
Rachel K <rkahn08@gmail.com>
2017-09-18 18:11:52 -0400

    everything works except item detail

--- src/ImageDetail.js
+++ src/ImageDetail.js
@@ -14,7 +14,7 @@ class ImageDetail extends Component {
 	componentDidMount() {
 
 		console.log(this.props.match);
-	    fetch('http://circuslabs.net/~michele.james/php/handle_fetch.php?type=detail&query=' + this.props.match.params.id,{
+	    fetch('http://circuslabs.net/~michele.james/php/handle_fetch.php?type=getDetails&query=' + this.props.match.params.id,{
 
 	            method: 'GET',
 	            headers: {
@@ -37,31 +37,30 @@ class ImageDetail extends Component {
 	render() {
 
 		return (
-
+			<div className="Item__Details--Container">
+				<h2 className="Gallery__Item--Title">{this.state.data.title}</h2>
+				<h6 className="Item__Score">{this.state.data.score}</h6>
+				<h6>{this.state.data.username}</h6>
+				<h6>{this.state.data.date}</h6>
+				<h4>{this.state.data.description}</h4>
+				<div className="Item__Image--Container">
+					<div className="Image--Overflow-setup">
+						{<img className="Item__Image" src={`http://circuslabs.net/~michele.james/php/thumbnails/${this.state.data.thumblink}`} alt={this.state.data.description}/>}
+					</div>
+				</div>
+			</div>
 			
 			
-			<div></div>	
 		)
 	}
 }
 
 export default ImageDetail;
 
+
+//
+				//
+
 //<GalleryItem  key={this.state.data.id} {...this.state.data} className="Container--Gallery__Item"/>
 
 
-// <div className ="Container--Gallery__Item detail">
-				
-// 				<div className="Item__Details--Container">
-// 				<h2 className="Gallery__Item--Title">{this.state.data.title}</h2>
-// 					<h6 className="Item__Score">{this.state.data.score}</h6>
-// 					<h6>{this.state.data.username}</h6>
-// 					<h6>{this.state.data.date}</h6>
-// 					<h4>{this.state.data.description}</h4>
-// 				</div>
-// 				<div className="Item__Image--Container">
-// 					<div className="Image--Overflow-setup">
-// 						{<img className="Item__Image" src={`http://circuslabs.net/~michele.james/php/thumbnails/${this.state.data.thumblink}`} alt={this.state.data.description}/>}
-// 					</div>
-// 				</div>
-// 			</div>
